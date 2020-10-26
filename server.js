56b7aab487542405ef3e3dfcac5e2f9ae3da46f5
nick <r951236958@gmail.com>
2020-10-26 11:58:40 +0800

    新增dist/mobile.css

--- server.js
+++ server.js
@@ -3,11 +3,15 @@
 const express = require('express');
 
 const dotenv = require('dotenv');
-require('dotenv').config();
+require('dotenv')
+    .config();
 
 const app = require('./app');
 
+const PORT = process.env.PORT || 3000;
+
 const server = app.listen(process.env.PORT || 3000, () => {
-  //console.log(`App listening on port http://localhost:${server.address().port}`);
-  console.log(`App listening on port ${server.address().port}`);
+    //const server = app.listen(process.env.PORT || 3000, () => {
+    //console.log(`App listening on port http://localhost:${server.address().port}`);
+    console.log(`Server listening on port :${server.address().port} `);
 });
