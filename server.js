80d9ed091696cb5309212dfbc4cd69ffa59c9ab0
r951236958 <r951236958@gmail.com>
2020-10-26 08:18:14 +0800

    server listen port setting

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-
-const server = app.listen(3000, () => {
-  console.log(`App listening on port http://localhost:${server.address().port}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+  //console.log(`App listening on port http://localhost:${server.address().port}`);
+  console.log(`App listening on port ${server.address().port}`);
 });
