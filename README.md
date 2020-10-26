e0b19363f5235e827c931c037ef0a22f2f677305
nick <r951236958@gmail.com>
2020-10-26 16:57:17 +0800

    change google oauth-client-id

---
+++ README.md
@@ -0,0 +1,7 @@
+# login-ejs
+
+Google Console Project: Quickstart-for-sheets
+
+Heroku App: login-ejs
+
+GitHub: login-ejs
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const dotenv = require("dotenv");
-require("dotenv").config();
-
+require("dotenv")
+    .config();
 
 /* GET home page. */
 //router.get('/', function (req, res, next) {
 //    res.render('index', { title: 'Express' });
 //});
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('layout', { title: 'Login' });
 });
 
